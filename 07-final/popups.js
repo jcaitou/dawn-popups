fddf66c702ce18e0284c3cb53b46080ba1152c70
jcaitou <71485747+jcaitou@users.noreply.github.com>
2023-02-23 13:45:55 +0800

    Add files via upload

---
+++ 07-final/popups.js
@@ -0,0 +1,40 @@
+
+  const pageType = "{{ request.page_type }}";
+  const modal = document.querySelector(".modal-container");
+
+  //close modal:
+    modal.addEventListener("click", (event) => {
+      const modalContent = event.target.querySelector(".modal-content");
+      const exitButton = event.target.closest("[data-action='close-popup']");
+        console.log(modalContent);
+
+      if (exitButton || modalContent) {
+        //we clicked outside the modal, so we should close the modal
+        modal.classList.add("out");
+      }
+    });
+
+//open modal:
+window.addEventListener("load", (event) => {
+  if (modal) {
+    console.log(modal.dataset.delay);
+    console.log(modal.dataset.show_once);
+    const popupDelay = parseInt(modal.dataset.delay);
+    const showOnce = modal.dataset.show_once === "true";
+    if (window.location.hash === "#newsletter-popup" && pageType !== "captcha") {
+        openPopup(modal);
+    } else if ( showOnce && localStorage.getItem("popupShown") === null ||
+         !showOnce ) {
+          setTimeout(
+            openPopup(modal),
+            popupDelay * 1000
+          );
+    }
+  }
+});
+
+function openPopup(element) {
+  modal.classList.remove("modal-animation","out");
+  modal.classList.add("modal-animation");
+  localStorage.setItem("popupShown", "true");
+}
